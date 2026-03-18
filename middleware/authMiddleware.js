01e2bab670d152ca85f26e0d30672deb0ee9857a
bennetfrankline02 <bennetfrankline02@gmail.com>
2026-03-18 15:42:20 +0530

    success

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,44 @@
+// const jwt = require("jsonwebtoken");
+
+// module.exports = function (req, res, next) {
+//   const token = req.header("Authorization");
+
+//   if (!token) {
+//     return res.status(401).json({ message: "Access Denied" });
+//   }
+
+//   try {
+//     const verified = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = verified;
+//     next();
+//   } catch (err) {
+//     res.status(400).json({ message: "Invalid Token" });
+//   }
+// };
+
+
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).json({ message: "Access Denied. No Token Provided" });
+  }
+
+  try {
+
+    const verified = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
+
+    req.user = verified;
+
+    next();
+
+  } catch (err) {
+    res.status(400).json({ message: "Invalid Token" });
+  }
+
+};
+
+module.exports = authMiddleware;
